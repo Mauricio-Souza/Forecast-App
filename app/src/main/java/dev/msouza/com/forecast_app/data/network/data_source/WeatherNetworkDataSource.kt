aee295b30ca9149171d16bd41541c297a4f3ebe1
Mauricio Souza <mauricioworkdev@outlook.com>
2019-01-26 19:08:53 -0200

    network data source created

---
+++ app/src/main/java/dev/msouza/com/forecast_app/data/network/data_source/WeatherNetworkDataSource.kt
@@ -0,0 +1,33 @@
+package dev.msouza.com.forecast_app.data.network.data_source
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import dev.msouza.com.forecast_app.data.network.response.CurrentWeatherResponse
+import dev.msouza.com.forecast_app.data.network.service.WeatherApiService
+import dev.msouza.com.forecast_app.intern.NoConnectivityException
+import kotlinx.coroutines.CoroutineExceptionHandler
+
+class WeatherNetworkDataSource (
+        private val weatherApiService: WeatherApiService
+) : WeatherDataSource {
+
+    private val _noConnectivity = MediatorLiveData<String>()
+    private val _downloadedCurrentWeather = MediatorLiveData<CurrentWeatherResponse>()
+
+    override val noConnectivity: LiveData<String>
+        get() = _noConnectivity
+
+    override val downloadedCurrentWeather: LiveData<CurrentWeatherResponse>
+        get() = _downloadedCurrentWeather
+
+    override suspend fun fetchCurrentWeather(location: String, languageCode: String) {
+        try {
+            val currentWeather = weatherApiService.getCurrentWeather(location, languageCode).await()
+            _downloadedCurrentWeather.postValue(currentWeather)
+        } catch (e: NoConnectivityException) {
+            Log.e("Error: ", e.message)
+            _noConnectivity.postValue(e.message)
+        }
+    }
+}
