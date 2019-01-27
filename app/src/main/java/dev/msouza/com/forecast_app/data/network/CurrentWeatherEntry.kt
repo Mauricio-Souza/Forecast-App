d3dc84256b8e917e2a48e7adf9c66f2a8e8fb966
Mauricio Souza <mauricioworkdev@outlook.com>
2019-01-27 20:54:52 -0200

    proporties changed in model

--- app/src/main/java/dev/msouza/com/forecast_app/data/network/CurrentWeatherEntry.kt
+++ app/src/main/java/dev/msouza/com/forecast_app/data/network/CurrentWeatherEntry.kt
@@ -2,16 +2,30 @@ package dev.msouza.com.forecast_app.data.network
 
 import com.google.gson.annotations.SerializedName
 
-data class CurrentWeatherEntry (
-        @SerializedName("last_update_epoch")
-        val lastUpdateEpoch: Int,
-        @SerializedName("last_update")
-        val lastUpdate: String,
+data class CurrentWeatherEntry(
         @SerializedName("temp_c")
         val tempC: Int,
         @SerializedName("temp_f")
         val tempF: Double,
         @SerializedName("is_day")
         val isDay: Int,
-        val condition: Condition
+        val condition: Condition,
+        @SerializedName("wind_mph")
+        val windMph: Double,
+        @SerializedName("wind_kph")
+        val windKph: Double,
+        @SerializedName("wind_dir")
+        val windDir: String,
+        @SerializedName("precip_mm")
+        val precipMm: Double,
+        @SerializedName("precip_in")
+        val precipIn: Double,
+        @SerializedName("feelslike_c")
+        val feelslikeC: Double,
+        @SerializedName("feelslike_f")
+        val feelslikeF: Double,
+        @SerializedName("vis_km")
+        val visKm: Double,
+        @SerializedName("vis_miles")
+        val visMiles: Double
 )
