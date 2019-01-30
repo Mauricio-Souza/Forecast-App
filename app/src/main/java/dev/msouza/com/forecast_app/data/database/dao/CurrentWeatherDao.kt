c7598e9e71cbd3b90a3f057e54aedf1b4af72073
Mauricio Souza <mauricioworkdev@outlook.com>
2019-01-30 20:37:33 -0200

    repository created

--- app/src/main/java/dev/msouza/com/forecast_app/data/database/dao/CurrentWeatherDao.kt
+++ app/src/main/java/dev/msouza/com/forecast_app/data/database/dao/CurrentWeatherDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import dev.msouza.com.forecast_app.data.database.entities.CURRENT_WEATHER_ID
+import dev.msouza.com.forecast_app.data.database.entities.CurrentWeatherEntry
 import dev.msouza.com.forecast_app.data.database.entities_mapper.ImperialCurrentWeatherEntry
 import dev.msouza.com.forecast_app.data.database.entities_mapper.MetricCurrentWeatherEntry
 import dev.msouza.com.forecast_app.data.database.entities_mapper.UnitSpecificCurrentWeatherEntry
@@ -13,7 +14,7 @@ import dev.msouza.com.forecast_app.data.database.entities_mapper.UnitSpecificCur
 @Dao
 interface CurrentWeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertOrUpdate(currentWeatherEntry: UnitSpecificCurrentWeatherEntry)
+    fun insertOrUpdate(currentWeatherEntry: CurrentWeatherEntry)
 
     @Query("select * from current_weather where id = $CURRENT_WEATHER_ID")
     fun getWeatherMetric() : LiveData<MetricCurrentWeatherEntry>
