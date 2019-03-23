35b033ade6a902760c5e0f45b717e0cdc3c836a8
Mauricio Souza <mauricioworkdev@outlook.com>
2019-03-23 20:01:09 -0300

    current weather fragment created

--- app/src/main/java/dev/msouza/com/forecast_app/ui/ForecastApp.kt
+++ app/src/main/java/dev/msouza/com/forecast_app/ui/ForecastApp.kt
@@ -1,6 +1,7 @@
 package dev.msouza.com.forecast_app.ui
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dev.msouza.com.forecast_app.data.database.db_provider.ForecastDatabase
 import dev.msouza.com.forecast_app.data.network.ConnectivityInterceptor
 import dev.msouza.com.forecast_app.data.network.ConnectivityInterceptorImpl
@@ -24,10 +25,15 @@ class ForecastApp : Application(), KodeinAware {
 
         bind() from singleton { ForecastDatabase(instance()) }
         bind() from singleton { instance<ForecastDatabase>().weatherDao() }
-        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(this@ForecastApp) }
         bind() from singleton { RetrofitProvider(instance()) }
         bind<WeatherDataSource>() with singleton { WeatherNetworkDataSource(instance()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
         bind() from provider { CurrentWeatherViewModelFactory(instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
     }
 }
